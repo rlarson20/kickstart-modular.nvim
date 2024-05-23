f561dd7cd3bf5f96f0af74095620d6816e842d0b
rlarson20 <90066470+rlarson20@users.noreply.github.com>
2024-05-23 00:02:29 -0400

    adding some useful plugs; codeium, obs, and zen-mode configuration of said plugs to come

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -12,6 +12,7 @@
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  -- TODO: add other tpoop plugs here
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
---
+++ todo.md
@@ -0,0 +1,2 @@
+# plugins to add/look into
+
