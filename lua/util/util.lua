27f6020f619ed64e28ce836cdc13552b57a8ec22
rlarson20 <richard.rj.larson@gmail.com>
2025-06-09 16:14:29 -0400

    new: added image.nvim in prep for adding molten for jupyter support, also removed unused util function

--- lua/util/util.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
--- snagged from alexjercan nvim dots, assume it's just a check for if module exists
-M.has = function(name)
-  local ok, _ = pcall(require, name)
-  return ok
-end
-
-return M
